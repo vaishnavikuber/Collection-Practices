03bf20fe6024e2e19258af73fd8f523101ce2aa9
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 14:18:18 +0530

    dictionary and stack and queue

--- CollectionPractices/GenericCollections.cs
+++ CollectionPractices/GenericCollections.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -22,8 +23,71 @@ namespace CollectionPractices
 
         public static void DictionaryFunction()
         {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 243);
+            d.Add('h', 876);
+            d.Add('k', 78);
+            d.Add('o', 234);
+
+            foreach (KeyValuePair<char, int> keyValue in d)
+            {
+                Console.WriteLine(keyValue.Key + "    " + keyValue.Value);
+            }
+        }
+
+
+        public static void SortedListFunction()
+        {
+            SortedList<int, char> sl = new SortedList<int, char>();
+            sl.Add(12, 'a');
+            sl.Add(22, 'v');
+            sl.Add(11, 'g');
+            sl.Add(32, 'r');
+
+            foreach (KeyValuePair<int, char> element in sl)
+            {
+                Console.WriteLine(element.Key + "    " + element.Value);
+            }
+        }
+
+        public static void StackFunction()
+        {
+            Stack<double> s = new Stack<double>();
+            s.Push(23.453);
+            s.Push(435.09);
+            s.Push(1323);
+            s.Push(6.5);
+            s.Push(20.09);
+
+            s.Pop();
+
+            foreach (double element in s)
+            {
+                Console.WriteLine(element);
+
+            }
 
         }
 
+
+        public static void QueueFunction()
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue("fdgfdv");
+            q.Enqueue("j");
+            q.Enqueue("Dog");
+            q.Enqueue("34.555564");
+
+
+
+            q.Dequeue();
+
+            foreach (string element in q)
+            {
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
     }
 }
