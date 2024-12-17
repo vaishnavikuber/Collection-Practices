0a4396576efbce4b78f642225e1a91ea53e10dbf
vaishnavi <vaishnavikuber2712@gmail.com>
2024-12-17 11:41:01 +0530

    collection programs

---
+++ CollectionPractices/NonGenericCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionPractices
+{
+    internal class NonGenericCollection
+    {
+        public static void ArrayListFunction()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(10);
+            al.Add("vaishnavi");
+            al.Add('c');
+            al.Add(29.67);
+            al.Add(DateTime.Now);
+
+            foreach(var element in al)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+
+    }
+}
---
+++ CollectionPractices/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CollectionPractices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------Collections--------------");
+            NonGenericCollection.ArrayListFunction();
+
+            Console.ReadLine();
+        }
+    }
+}
